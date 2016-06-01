7db03a3c7d348c45dc797a73947a91fae84f3ccb
land-pack <landpack@sina.com>
2016-06-01 13:47:56 +0800

    Build# Base framework

--- app/gallery/celeryconfig.py
+++ app/gallery/celeryconfig.py
@@ -1,6 +1,6 @@
 CELERY_IMPORTS = ('tasks')
 CELERY_IGNORE_RESULT = False
-BROKER_HOST = "192.168.1.28" # IP address of the server running RabbitMQ and Celery
+BROKER_HOST = "127.0.0.1" # IP address of the server running RabbitMQ and Celery
 BROKER_PORT = 5672
 BROKER_URL ='amqp://'
 CELERY_RESULT_BACKEND = "amqp"
